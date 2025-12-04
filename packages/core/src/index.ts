35df3ce3d0ef0f770b6ffb6a6518709fd8109742
Fritz Benning <mail@fritzbenning.de>
2025-12-04 22:07:34 +0100

    refactor: remove unnecessary whitespace and streamline exports in index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,7 @@
 import "@/lib/styles/styles.css";
 
 export { createCanvasObserver } from "@/lib/canvas/createCanvasObserver";
-
 export { createNodeTools } from "@/lib/node-tools/createNodeTools";
 export type { NodeTools, NodeToolsRef } from "@/lib/node-tools/types";
-
 export { createViewport } from "@/lib/viewport/createViewport";
 export type { Viewport, ViewportRef } from "@/lib/viewport/types";
